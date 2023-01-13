d9ac0385ad69108a3aaa611c6f6ca2337272c391
Mohamed Afify <mohamed.ae1717@gmail.com>
2023-01-13 23:52:00 +0200

    firt chart

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from '../Components/Card'
 import Navbar from '../Components/Navbar'
-
+import Chart1 from '../Components/Chart1'
 const Dashboard = () => {
   return (
     <div className='First-line'>
       <Card/>
+      <Chart1/>
+      
     </div>
   )
 }

95b987cd0238c492f9a9771526ade5c07b18fb2d
Mohamed Afify <mohamed.ae1717@gmail.com>
2023-01-13 20:36:12 +0200

    Navbar

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 const Dashboard = () => {
   return (
     <div>
+      <Navbar/>
         <h1>this is Dashboard</h1>
     </div>
   )
